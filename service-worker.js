bbc646f1051edab8af2f168b340cecf68447ed0b
noapark94-blip <noapark94@gmail.com>
2026-07-27 03:29:09 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hansalmae-voca-v34';
+const CACHE_NAME = 'hansalmae-voca-v35';
 const REQUIRED_ASSETS = ['./', './index.html', './manifest.json'];
 const OPTIONAL_ASSETS = ['./teacher.html', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function (event) {
